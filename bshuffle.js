ce3bab323bae66adc0cab1615718e1746e37ff70
risonakamo <hmykanae@gmail.com>
2017-09-12 19:00:35 -0400

    errors are happening

--- bshuffle.js
+++ bshuffle.js
@@ -2,11 +2,20 @@ class _bshuffle
 {
     /*
         --- variables ---
-        element-array songs: array of clickable divs used to start songs
+        element-array songs: array of clickable divs used to start songs. not a nodelist.
+        mutationobserver timewatcher
     */
     constructor()
     {
-        this.songs=document.querySelectorAll(".play_status");
+        this.songs=[];
+        var e_songs=document.querySelectorAll(".play_status");
+
+        for (var x=0,l=e_songs.length;x<l;x++)
+        {
+            this.songs.push(e_songs[x]);
+        }
+
+        this.playrandom();
     }
 
     //hook end time and begin observing current time for when it matches end time
@@ -15,16 +24,18 @@ class _bshuffle
         var e_curtime=document.querySelector(".time_elapsed");
         var fintime=document.querySelector(".time_total").innerText;
 
-        var timewatcher=new MutationObserver((m)=>{
+        console.log(fintime);
+
+        this.timewatcher=new MutationObserver((m)=>{
             if (m[1].addedNodes[0].data==fintime)
             {
                 console.log("%cbshuffle","color:#b578cc","song end");
-                timewatcher.disconnect();
+                this.timewatcher.disconnect();
                 this.playrandom();
             }
         });
 
-        timewatcher.observe(e_curtime,{childList:true});
+        this.timewatcher.observe(e_curtime,{childList:true});
     }
 
     //play random song and remove it, also runs songstart operations
@@ -35,10 +46,16 @@ class _bshuffle
             return;
         }
 
+        if (this.timewatcher)
+        {
+            this.timewatcher.disconnect();
+        }
+
+        console.log(this.songs);
         var randomsong=Math.floor(Math.random()*this.songs.length);
 
         this.songs[randomsong].click();
-        this.songs.splice(randomsong,0);
+        this.songs.splice(randomsong,1);
         this.songstart();
     }
 }
